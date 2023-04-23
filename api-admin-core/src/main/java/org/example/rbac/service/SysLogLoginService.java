5e3849f6b16fb83d87ed9aa1eb3bc7e69e349093
xl123-ss <3179716692@qq.com>
2023-04-23 22:28:37 +0800

    feat(custom): 登录事件监听，保留登录日志，登录日志查询

--- api-admin-core/src/main/java/org/example/rbac/service/SysLogLoginService.java
+++ api-admin-core/src/main/java/org/example/rbac/service/SysLogLoginService.java
@@ -1,8 +1,11 @@
 package org.example.rbac.service;
 
 
+import org.example.common.utils.PageResult;
 import org.example.mybatis.service.BaseService;
 import org.example.rbac.entity.SysLogLoginEntity;
+import org.example.rbac.query.SysLogLoginQuery;
+import org.example.rbac.vo.SysLogLoginVO;
 
 /**
  * 登录日志业务接口
@@ -10,5 +13,20 @@ import org.example.rbac.entity.SysLogLoginEntity;
  * @author mqxu
  **/
 public interface SysLogLoginService extends BaseService<SysLogLoginEntity> {
+    /**
+     * 按条件分页查询
+     *
+     * @param query 查询参数
+     * @return 分页结果
+     */
+    PageResult<SysLogLoginVO> page(SysLogLoginQuery query);
 
+    /**
+     * 保存登录日志
+     *
+     * @param username  用户名
+     * @param status    登录状态
+     * @param operation 操作信息
+     */
+    void save(String username, Integer status, Integer operation);
 }
