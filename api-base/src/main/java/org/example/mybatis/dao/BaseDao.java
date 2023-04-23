3c1f37d2bca03293a29f98b54da083ce6327a77a
xl123-ss <3179716692@qq.com>
2023-04-23 19:09:39 +0800

    feat(custom): 基础核心模块mp相关配置

---
+++ api-base/src/main/java/org/example/mybatis/dao/BaseDao.java
@@ -0,0 +1,12 @@
+package org.example.mybatis.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 基础Dao
+ *
+ * @author
+ */
+public interface BaseDao<T> extends BaseMapper<T> {
+
+}
