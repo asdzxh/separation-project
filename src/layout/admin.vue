19831616c129ee49861da64e5f63c44811e3478b
asdzxh <2832009432@qq.com>
2023-04-25 14:57:24 +0800

    feat(custom): keep-alive页面缓存和transition全局过渡动画

--- src/layout/admin.vue
+++ src/layout/admin.vue
@@ -11,7 +11,13 @@
 
       <el-main>
         <bread-crumbs />
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="fade">
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+          </transition>
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -29,9 +35,28 @@ const { sideWidth } = storeToRefs(store)
 <style scoped>
 .el-main {
   @apply w-full h-screen text-left;
+  padding: 0;
 }
-
 .el-aside {
   transition: all 0.2s;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+.fade-enter-active {
+  transition-delay: 0.3s;
 }
 </style>
